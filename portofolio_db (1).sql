d73c6fb78eb92a0c81df548cb29ee84996dd99ef
Ferdi Seswanto <ferdi.seswanto@gmail.com>
2026-02-07 00:24:08 +0800

    Database Fix

--- portofolio_db (1).sql
+++ portofolio_db (1).sql
@@ -146,7 +146,7 @@ CREATE TABLE `project` (
   `updated_at` timestamp NULL DEFAULT NULL,
   `tech_stack` varchar(255) DEFAULT NULL,
   `tahun` year DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `project`
